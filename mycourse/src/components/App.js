1dd5e522ccc802e2f5c82d9980bab1a23dedf291
thetoon30 <thetoon30@gmail.com>
2020-12-12 09:22:12 +0700

    component contentTab

--- mycourse/src/components/App.js
+++ mycourse/src/components/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ContentBannar from './contentBanner.js';
 import ContentCourse from './contentCourse.js';
+import ContentTab from './contentTab.js';
 
 function App() { 
   return (
     <div id="course-detail-page" className="theme-skilllane">
       <ContentBannar></ContentBannar>
       <ContentCourse></ContentCourse>
+      <ContentTab></ContentTab>
     </div>
   );
 }
---
+++ mycourse/src/components/contentTab.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function ContentTab() {
+    
+    return (
+        <div id="courseDetailContent" className="content-course">
+        <div className="container">
+          <div className="row content-course-row">
+            <div className="col-lg-9 col-xs-12 course-detail-card -padding">
+              <div role="tabpanel" className="content-course-tabs">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default ContentTab;
